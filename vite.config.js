f76357ea70baf4b23792da73d5f6c34c802f24e5
conrad-strughold <michael.van.paesschen@gmail.com>
2025-10-05 15:49:54 +0100

    custom domain: base=/ and CNAME

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/rivensite/',      // repo name for GitHub Pages
-  build: { outDir: 'docs' } // Pages will serve /docs on main
+  base: '/',               // <-- change from '/rivensite/' to '/'
+  build: { outDir: 'docs' }
 })
