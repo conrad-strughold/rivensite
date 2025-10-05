37646d15f16a38114b57753bfde461522f579796
conrad-strughold <michael.van.paesschen@gmail.com>
2025-10-05 20:10:25 +0100

    update site build

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/rivensite/',               // <-- change from '/rivensite/' to '/'
-  build: { outDir: 'docs' }
+  base: './',        // <-- IMPORTANT for custom domain riven.finance
+  build: {
+    outDir: 'docs',
+    assetsDir: 'assets',
+  },
 })
